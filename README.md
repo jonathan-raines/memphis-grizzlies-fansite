804c66bce2448ea70d42033af2605f8a2bfc0ebd
Jonathan Raines <jonathan.s.raines@gmail.com>
2020-11-23 16:31:51 -0600

    added existing source files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# memphis-grizzlies-fansite
+# CS290-Grizzlies-FanPage
+Website created for Web Development Assignment. It is a pseudo fan page /team page for the Memphis Grizzlies
---
+++ grizzlies.js
@@ -0,0 +1,52 @@
+/***************************************************************************************************
+ * Name: Jonathan Raines
+ * Date: November 14, 2019
+ * CS 290 Project: Create a team page for the Memphis Grizzlies, which will show stats, 
+ * schedule, and links to external sports sources
+ **************************************************************************************************/
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 61990);
+
+app.use(express.static('public'));      // https://www.youtube.com/watch?v=o4njTeKjGWQ
+
+app.get('/', function(req,res) {
+    res.render('home');
+});
+
+app.get('/schedule', function(req,res) {
+    res.render('schedule');
+});
+
+app.get('/rookies', function(req,res) {
+    res.render('rookies');
+});
+
+app.get('/facts', function(req,res) {
+    res.render('facts');
+});
+
+app.get('/subscribe', function(req,res) {
+    res.render('subscribe');
+});
+
+app.use(function(req,res) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(req,res) {
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press CTRL-C to terminate.');
+});
